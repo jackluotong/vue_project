daca840723c3b2b7b70f1692ad92e28122738311
LAPTOP-7U2RL3VE\WilliamLuo <jackluotong@outlook.com>
2020-12-04 17:23:37 +0800

    for interview add some knoeledge.

--- src/components/DecemberInterview.vue
+++ src/components/DecemberInterview.vue
@@ -7,23 +7,33 @@
       <div class="content"> 
         <ol>
           <li v-for="(item,index) in topic1202" :key='index' class="liClass">
-           {{item.index}}{{item.name}}；答案：<span style="color:blue">{{item.answer}} </span>
+           {{item.index}}{{item.name}}<br>答案：<span style="color:blue">{{item.answer}}. </span>
           </li>
         </ol>
         <a-button @click="testAxios">test axios</a-button>
       </div>
       </a-card>
        <a-card
-     title="2020-12-02 Interivew|无锡"
+     title="2020-12-03 Interivew|Begin"
       :bordered="false"
       style="width:98%;margin:0 auto">  
       <div class="content"> 
         <ol>
           <li v-for="(item,index) in another" :key='index' class="liClass">
-           {{item.index}}{{item.name}}；答案：<span style="color:blue">{{item.answer}} </span>
+           {{item.index}}{{item.name}}<br>答案：<span style="color:blue">{{item.answer}}. </span>
           </li>
         </ol>
        
+      </div>
+      </a-card>
+       <a-card
+     title="HTML/CSS Operate"
+      :bordered="false"
+      style="width:98%;margin:0 auto">
+      <div class="content"> 
+           <div class="centerVerticalHorizontal">
+              <p><a href="https://blog.csdn.net/weixin_37580235/article/details/82317240#%E5%B7%B2%E7%9F%A5%E9%AB%98%E5%BA%A6%E5%92%8C%E5%AE%BD%E5%BA%A6%E7%9A%84%E5%85%83%E7%B4%A0"> block element/located in center</a></p>
+          </div>
       </div>
       </a-card>
   </div>
@@ -31,6 +41,7 @@
 <script>
 var arr=[[1,2],[3,4]];
 var arrSet=[1,12,212,1,2,2,1,1,12,21,2122113,1,null,undefined,null]
+ const nums=[12,12,122,1,11,23,232]
 export default {
   data(){
     return{
@@ -44,10 +55,25 @@ export default {
        ],
       another:[
         {name:"从输入URL到页面展示发生了什么？",answer:""},
+        {name:"12-03：水平垂直居中？",answer:""},
+        {name:"12-03：es6中数组和对象新增的方法？",answer:""},
+        {name:"12-04: 值类型和引用类型的区别？",answer:""},
+        {name:"12-03：深拷贝和浅拷贝？",answer:"浅拷贝就是对对象进行浅层次的复制，只复制一层对象的属性，并不包括对象里面的引用类型数据 深拷贝是对对象以及对象的所有子对象进行拷贝，也就是说新拷贝对象的子对象里的属性也不会影响到原来的对象"},
+        {name:"12-04: 值类型和引用类型的区别？",answer:"值类型的数据存储在栈中，函数运行完成后变量销毁，复制是深复制，不能添加属性和方法，值的比较。|引用类型的存储在堆中，运行完不会销毁，复制是浅复制，可添加属性和方法，引用地址的比较。"},
+        {name:"12-04: 事件循环机制?(宏任务/微任务)",answer:"宏任务：script主代码，setTimeout,setInterval,I/O,UI渲染/微任务：promise.then"},
+        {name:"12-04: 行内元素和块级元素的区别？",answer:"行内元素都在水平方向上排列/块级元素都在垂直方向排列，行内元素盒模型属性不同/行内(span,a,i,b)块级(p,h1,table,div,li)"},
+        {name:"12-04: 跨域问题？为什么会有跨域问题？怎么解决？",answer:"因为浏览器的同源策略(协议域名端口相同)才会出现跨域问题/jsonp,cors,nginx反向代理接口跨域,node.js中间件代理跨域,location.hash+iframe,window.name+iframe,postMessage,WebSocket"},
+        {name:"12-04: 浏览器兼容性问题？为什么？怎么办？",answer:""},
+        {name:"12-04: 回流和重绘？为什么？怎么办？",answer:"回流是元素的规模尺寸，布局，隐藏等改变而需要重新构建/重绘元素的外观，风格，而不会影响布局的"},
         {name:"",answer:""},
-        {name:"",answer:""}
       ]
     }
+  },
+  computed:{
+
+  },
+  watch:{
+
   },
   mounted(){
     var str=" ad da ad   "
@@ -68,6 +94,14 @@ export default {
   },
   created(){
     console.log(this.checkType('122'));
+    let name=localStorage.key(1);
+    let value=localStorage.getItem(name);
+    console.log(value);
+    this.filterFun();
+    this.mapFun();
+    this.reduceFun();
+    this.allFun();
+    this.constructionFun()
   },
   methods:{ 
     /**
@@ -124,8 +158,73 @@ export default {
       return Object.prototype.toString.call(params)
     },
     /**
-     * 
+     * 深拷贝和浅拷贝
      */
+
+    /**
+     * filter filter the data from your condition
+     */
+    filterFun:function(){
+       let newNums=nums.filter(n=>{
+        return n<12;
+      });
+      console.log(newNums);
+    },
+    /**
+     * map
+     */
+    mapFun:function(){
+      let newNums=nums.map(n=>{
+        return n*10;
+      })
+      console.log(newNums);
+    },
+    /**
+     * reduce
+     */
+    reduceFun:function(){
+      let newNums=nums.reduce((preValue,n)=>{
+        return preValue+n;
+      },0)
+      console.log(newNums);
+    },
+    /**
+     * all reduce+fliter+map
+     */
+    allFun:function(){
+      let all=nums.filter(n=>n<100).map(n=>n*2).reduce((pre,n)=>pre+n)
+      console.log(all);
+    },
+    /**
+     * construction evaluation
+     */
+    constructionFun:function(){
+      const point=[12,234,2]
+      const [x,y,z]=point
+      console.log(x,y,z);
+
+      /**
+       * for of 
+       */
+      for(const num of nums){
+        console.log('for of +'+num);
+      }
+      /**
+       * includes 
+       * setTimeout(function(),timer)
+       */
+      if(nums.includes(1223))
+      {
+        console.log('includes 1223');
+      }else{
+        setTimeout(()=>{this.$message({
+          type:'default',
+          message:'not include.'
+        })},1000) 
+      }
+      clearTimeout()
+     },
+     
   }
 }
 </script>
@@ -135,5 +234,12 @@ export default {
 }
 .liClass{
 text-align: left;
+}
+.centerVerticalHorizontal{
+    /*
+    
+    */
+    justify-content: left;
+    display: flex;
 }
 </style>
