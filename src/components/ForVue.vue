e4cd2c0a7331118f75942ab48165353d666a9f3a
jackluotong <jackluotong@outlook.com>
2020-12-15 23:45:08 +0800

    use $refs to change the value that you inputed in the text and exert bind two ways.

--- src/components/ForVue.vue
+++ src/components/ForVue.vue
@@ -6,8 +6,14 @@
       style="width:98%;margin:0 auto"
     >
       <div class="content">
-        <input type="text" ref="input1" />
-        <button @click="add" ref="opt1">{{ value }}</button>
+        <input
+          type="text"
+          ref="input1"
+        />
+        <button
+          @click="add"
+          ref="opt1"
+        >{{ value }}</button>
       </div>
     </a-card>
     <a-card
@@ -28,7 +34,10 @@
       <div class="content">
         <p>{{ fullname }}</p>
         <a-divider>watch</a-divider>
-        <p>Ask a yes/no quesion: <input type="text" v-model="question" /></p>
+        <p>Ask a yes/no quesion: <input
+            type="text"
+            v-model="question"
+          /></p>
         <p>{{ answer }}</p>
         <p ref="old">new</p>
         <P ref="new">{{ old }}</P>
@@ -41,88 +50,115 @@
     >
       <div class="content">
         <p :style="styleObject">我是用对象做的样式绑定</p>
-         <a-divider>条件渲染</a-divider>
-         <p v-if="active">v-if</p>
-         <p>v-if vs v-show
-            v-if 是“真正”的条件渲染，因为它会确保在切换过程中条件块内的事件监听器和子组件适当地被销毁和重建。
+        <a-divider>条件渲染</a-divider>
+        <p v-if="active">v-if</p>
+        <p>v-if vs v-show
+          v-if 是“真正”的条件渲染，因为它会确保在切换过程中条件块内的事件监听器和子组件适当地被销毁和重建。
 
-            v-if 也是惰性的：如果在初始渲染时条件为假，则什么也不做——直到条件第一次变为真时，才会开始渲染条件块。
+          v-if 也是惰性的：如果在初始渲染时条件为假，则什么也不做——直到条件第一次变为真时，才会开始渲染条件块。
 
-            相比之下，v-show 就简单得多——不管初始条件是什么，元素总是会被渲染，并且只是简单地基于 CSS 进行切换。
+          相比之下，v-show 就简单得多——不管初始条件是什么，元素总是会被渲染，并且只是简单地基于 CSS 进行切换。
 
-            一般来说，v-if 有更高的切换开销，而 v-show 有更高的初始渲染开销。因此，如果需要非常频繁地切换，
-            则使用 v-show 较好；如果在运行时条件很少改变，则使用 v-if 较好。
-          </p>
-      <p>v-for 具有比 v-if 更高的优先级</p>
+          一般来说，v-if 有更高的切换开销，而 v-show 有更高的初始渲染开销。因此，如果需要非常频繁地切换，
+          则使用 v-show 较好；如果在运行时条件很少改变，则使用 v-if 较好。
+        </p>
+        <p>v-for 具有比 v-if 更高的优先级</p>
       </div>
     </a-card>
 
-     <a-card
+    <a-card
       title="列表渲染"
       :bordered="false"
       style="width:98%;margin:0 auto"
     >
       <div class="content">
-         <ul>
-           <li v-for="(item,index) in items" :key="index">
+        <ul>
+          <li
+            v-for="(item,index) in items"
+            :key="index"
+          >
             {{index}}:{{item.name}}
-           </li>
-         </ul>
-         <ul>
-           <li v-for="(value,name,index) in object" :key="index">
-               {{index}}:{{name}}:{{value}}
-           </li>
-         </ul>
-          <a-divider>数组</a-divider>
-          <p>push()
-              pop()
-              shift()
-              unshift()
-              splice()
-              sort()
-              reverse()</p>
+          </li>
+        </ul>
+        <ul>
+          <li
+            v-for="(value,name,index) in object"
+            :key="index"
+          >
+            {{index}}:{{name}}:{{value}}
+          </li>
+        </ul>
+        <a-divider>数组</a-divider>
+        <p>push()
+          pop()
+          shift()
+          unshift()
+          splice()
+          sort()
+          reverse()</p>
       </div>
     </a-card>
-     <a-card
+    <a-card
       title="事件处理"
       :bordered="false"
       style="width:98%;margin:0 auto"
     >
       <div class="content">
-          <a-button type="primary" @click="clickOne">
-             Primary
-         </a-button>
-         <a-button type="primary" @click="say('what')">
-             Primary
-         </a-button>
-            <a-divider>事件修饰符</a-divider>
-       <p>.stop/prevent/capture/self/once/passive</p>
+        <a-button
+          type="primary"
+          @click="clickOne"
+        >
+          Primary
+        </a-button>
+        <a-button
+          type="primary"
+          @click="say('what')"
+        >
+          Primary
+        </a-button>
+        <a-divider>事件修饰符</a-divider>
+        <p>.stop/prevent/capture/self/once/passive</p>
       </div>
     </a-card>
-    <a-card 
+    <a-card
       title="表单输入绑定"
       :bordered="false"
-      style="width:98%;margin:0 auto">
+      style="width:98%;margin:0 auto"
+    >
       <div class="content">
         <p>{{messageOne}}</p>
-        <input type="text" v-model="messageOne" placeholder="edit me">
-        <input type="checkbox" id="jack" value="jack" v-model="checkedNames">
+        <input
+          type="text"
+          v-model="messageOne"
+          placeholder="edit me"
+        >
+        <input
+          type="checkbox"
+          id="jack"
+          value="jack"
+          v-model="checkedNames"
+        >
         <label for="jack">jack</label>
-        <input type="checkbox" id="jack" value="jackluo" v-model="checkedNames">
+        <input
+          type="checkbox"
+          id="jack"
+          value="jackluo"
+          v-model="checkedNames"
+        >
         <label for="jack">jackluo</label>
         <hr>
         <span>checked names:{{checkedNames}}</span>
-       </div>
+      </div>
     </a-card>
     <a-card
-     title="深入了解组件"
+      title="深入了解组件"
       :bordered="false"
       style="width:98%;margin:0 auto"
     >
       <div class="content">
         <p><a href="https://www.cnblogs.com/july-sunny/p/11687008.html">通过 Prop 向子组件传递数据</a></p>
         <p><a href="https://cn.vuejs.org/v2/guide/components-props.html#%E4%BC%A0%E9%80%92%E9%9D%99%E6%80%81%E6%88%96%E5%8A%A8%E6%80%81-Prop">
-        $emit</a></p>
+            $emit</a></p>
         <a-divider>Prop</a-divider>
         <h2>传递静态或动态 Prop</h2>
         <h2>单向数据流</h2>
@@ -132,25 +168,25 @@
       </div>
     </a-card>
     <a-card
-     title="动态组件 & 异步组件"
+      title="动态组件 & 异步组件"
       :bordered="false"
       style="width:98%;margin:0 auto"
     >
       <div class="content">
-         <button @click="show = !show">
-    Toggle render
-      </button>
-      <transition name="slide-fade">
-        <p v-if="show">hello</p>
-      </transition>
+        <button @click="show = !show">
+          Toggle render
+        </button>
+        <transition name="slide-fade">
+          <p v-if="show">hello</p>
+        </transition>
       </div>
     </a-card>
     <a-card
-    title="深入响应式原理"
+      title="深入响应式原理"
       :bordered="false"
       style="width:98%;margin:0 auto"
       @click="vueSenior"
-      >
+    >
       <div></div>
     </a-card>
   </div>
@@ -164,25 +200,25 @@ export default {
       lastName: 'William',
       question: '',
       answer: 'I cannot give you an answer until you ask a question!',
-      old: '', 
-      styleObject:{
-        color:'red',
-        fontsize:'40px',
-       },
-       active:true,
-       items:[//v-for 遍历数组 item in items，遍历对象 value in object
-         {name:'jack'},
-         {name:'william'}
-       ],
-       object:{
-         title:'man',
-         author:'jackluo',
-         publish:'sh'
-       },
-       name:'Vue.js',
-       messageOne:'hello',
-       checkedNames:[],
-       show:true,
+      old: '',
+      styleObject: {
+        color: 'red',
+        fontsize: '40px',
+      },
+      active: true,
+      items: [//v-for 遍历数组 item in items，遍历对象 value in object
+        { name: 'jack' },
+        { name: 'william' }
+      ],
+      object: {
+        title: 'man',
+        author: 'jackluo',
+        publish: 'sh'
+      },
+      name: 'Vue.js',
+      messageOne: 'hello',
+      checkedNames: [],
+      show: true,
     }
   },
   watch: {
@@ -191,8 +227,8 @@ export default {
       console.log('this is old name' + oldName)
     },
     question(newQuestion, oldQuestion) {
-        this.answer = 'Wating for you...'
-        this.$nextTick(function() {
+      this.answer = 'Wating for you...'
+      this.$nextTick(function () {
         this.$refs.new.value = newQuestion
         this.old = oldQuestion
         console.log('new value' + this.$refs.new.value)
@@ -201,7 +237,7 @@ export default {
     },
   },
   methods: {
-    add: function() {
+    add: function () {
       console.log(this.$refs.input1.value)
       let s = this.$refs.input1.value
       if (!s) {
@@ -211,27 +247,27 @@ export default {
         let that = this
         console.log(that)
         that.value = that.$refs.input1.value
-        this.$nextTick(function() {
+        this.$nextTick(function () {
           console.log(that.$refs.opt1.innerText)
         })
       }
     },
-    clickOne:function(event){
+    clickOne: function (event) {
       alert(this.name)
-      if(event){
+      if (event) {
         alert(event.target.tagName)
         console.log(event);
       }
     },
-    say:function(message){
+    say: function (message) {
       alert(message)
     },
-    vueSenior:function(){ 
+    vueSenior: function () {
       window.open('https://cn.vuejs.org/v2/guide/reactivity.html');
     }
   },
   computed: {
-    fullname: function() {
+    fullname: function () {
       return this.firstName + ',' + this.lastName
     },
   },
--- src/components/JsAdvanced.vue
+++ src/components/JsAdvanced.vue
@@ -1,11 +1,91 @@
 <template>
-  <div>
-    <p>12</p>
+  <div style="background:#ECECEC; padding:20px;">
+    <a-card
+      title="手写常见的API | 上部分"
+      :bordered="false"
+      style="width:98%;margin:0 auto"
+    >
+      <div class="content">
+        <div class="partOne">
+          <h4 class="textP">手写instanceOf </h4>
+        </div>
+        <div class="partTwo">
+          <a-input
+            class="input"
+            placeholder="请输入数据"
+            v-model="checkData"
+            ref="inputVlue"
+          />
+        </div>
+        <div>
+          <a-button
+            type="primary"
+            @click="showDrawer"
+          >
+            check
+          </a-button>
+          <a-drawer
+            title="Basic Drawer"
+            placement="right"
+            :closable="false"
+            :visible="visible"
+            :after-visible-change="afterVisibleChange"
+            @close="onClose"
+          >
+            <p ref="showText">1</p>
+          </a-drawer>
+        </div>
+      </div>
+    </a-card>
   </div>
 </template>
-
 <script>
 export default {
-  
+  data() {
+    return {
+      visible: false,
+      checkData: ''
+    }
+  },
+  mounted() {
+    console.log(this.myInstanceOf('1111', String));
+    console.log(this.myInstanceOf(new String("111"), String));
+    this.$nextTick(() => {
+      this.$refs.showText = '1222';
+      this.showText = '1111'
+    })
+
+  },
+  watch: {},
+  methods: {
+    myInstanceOf: function (left, right) {
+      if (typeof left !== 'object' || left == null) return false;
+      let proto = Object.getPrototypeOf(left);
+      if (proto == null) return false;
+      if (proto == right.prototype) return true;
+      proto = Object.getPrototypeOf(proto)
+    },
+    afterVisibleChange(val) {
+      console.log('visible', val);
+    },
+    showDrawer() {
+      this.visible = true;
+      //get value
+      let value = this.checkData;
+      //use function
+      this.myInstanceOf(value, typeof (value))
+      alert(this.$refs.showText)
+    },
+    onClose() {
+      this.visible = false;
+    },
+  }
 }
 </script>
+<style scoped>
+.partTwo,
+.partOne {
+  display: inline-block;
+  padding-left: 10mm;
+}
+</style>
