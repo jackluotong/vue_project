d52bd851d83651abeece94ccb5a0d38c30d780b6
LAPTOP-7U2RL3VE\WilliamLuo <jackluotong@outlook.com>
2021-03-09 17:57:50 +0800

    complete interview knowledge.

--- src/components/Interview/March.vue
+++ src/components/Interview/March.vue
@@ -17,37 +17,37 @@
             {{item.index}}{{item.title}} <span>题目：</span> {{item.name}}<br>答案：<span style="color:blue">{{item.answer}}. </span>
           </li>
         </ol>
-        <!-- <img :src='img'/> -->
+       <!-- <img :src='img'/> -->
       </div>
     </a-card>
 
     <!-- cliclk function -->
-    <a-card
+     <a-card
       title="2021-03 Interivew"
       :bordered="false"
       style="width:98%;margin:0 auto"
     >
       <div class="content">
-        <a-button
-          type='primary'
-          @click='promiseFun'
-          class="btn"
-        >promise test</a-button>
-        <a-button
-          type='primary'
-          @click='promiseChain'
-          class="btn"
-        >promise chain use</a-button>
+       <a-button 
+       type='primary' 
+       @click='promiseFun'
+       class="btn"
+       >promise test</a-button>
+        <a-button 
+       type='primary' 
+       @click='promiseChain'
+       class="btn"
+       >promise chain use</a-button>
       </div>
     </a-card>
     <!-- photo and other -->
-    <a-card
+     <a-card
       title="2021-03 Interivew"
       :bordered="false"
       style="width:98%;margin:0 auto"
     >
       <div class="content">
-        <img :src='img' />
+       <img :src='img'/>  
       </div>
     </a-card>
   </div>
@@ -69,65 +69,65 @@ export default {
         { name: 'promise原理以及all的时候需要传什么参数？', answer: 'promise使用回调函数，把回调函数封装在内部；all的时候把所有的promise放在数组中入参' },
         { name: '垂直水平居中的方式？', answer: '第一种父元素相对布局，子元素绝对布局+外边距，第二种flex布局，第三种父元素相对布局,子元素绝对布局+transform，第四种 表格' },
         { title: '03.02律商联讯(外资)', name: 'css样式优先级？', answer: '!important > 行内样式>ID选择器 > 类选择器 > 标签 > 通配符 > 继承 > 浏览器默认属性' },
-        { name: 'es5继承？', answer: '' },
-        { name: 'js中的宏任务和微任务？', answer: '' },
+        { name: 'es5继承？', answer: 'prototype实现，组合/寄生/' },
+        { name: 'js中的宏任务和微任务？', answer: 'marcotask——ui rendering i/o setTimeout setInterval  /microtask-promise process.nextTick' },
         { name: 'expert和expert default的区别？', answer: '' },
         { name: 'es6新特性？', answer: '' },
         { name: '数据库索引原理？索引加在哪里？sql怎么优化查询？', answer: '' },
-        { title: '03.04美团面试', name: 'package-lock.json是做什么的？需要上传到git嘛？', answer: '' },
-        { name: 'cdn和dns有什么区别？', answer: '' },
+        { title: '03.04美团面试', name: 'package-lock.json是做什么的？需要上传到git嘛？', answer: '是用来记录安装包的版本，需要上传' },
+        { name: 'cdn和dns有什么区别？', answer: 'cdn:内容分发网络,让用户能够更快速的得到请求的数据/dns:将域名转换为ip地址' },
         { name: 'css垂直居中和水平垂直居中？', answer: '' },
-        { name: '数组的降维？', answer: '' },
+        { name: '数组的降维？', answer: 'arr+= " "arr=arr.split(",")'},
         { name: 'prototype怎么应用？', answer: '' },
         { name: 'http常见状态码？1 2 3 4 5 ', answer: '' },
         { name: '为什么要用node作为中间件？', answer: '' },
         { name: 'Vue中组件的', answer: '' },
         { name: '事件循环？', answer: '' },
       ],
-      img: require('../../assets/mvvm.png')//做图片动态绑定的时候需要用require
+      img:require('../../assets/mvvm.png')//做图片动态绑定的时候需要用require
     }
   },
   methods: {
-    promiseFun: function () {
-      let run = function () {
-        let _promise = new Promise(function (resolve, reject) {
-          setTimeout(() => {
-            let rand = Math.random()
-            if (rand > 0.5) {
-              resolve('resolve' + rand)
-            } else {
-              reject('reject' + rand)
-            }
-          }, 1000);
-        })
-        return _promise
-      }
-      run()
+   promiseFun:function(){
+     let run=function(){
+     let _promise=new Promise(function(resolve,reject){
+       setTimeout(() => {
+         let rand=Math.random()
+         if(rand>0.5){
+           resolve('resolve'+rand)
+         }else{
+           reject('reject'+rand)
+         }
+       }, 1000);
+     })
+     return _promise
+   }
+   run()
     },
-    promiseChain: function () {
-      function run_a() {
-        return new Promise(function (resolve,) {
+    promiseChain:function(){
+      function run_a(){
+        return new Promise(function(resolve,){
           resolve('step1')
         })
       }
-      function run_b(data_a) {
-        return new Promise(function (resolve,) {
-          console.log(data_a);
+      function run_b(data_a){
+         return new Promise(function(resolve,){
+           console.log(data_a);
           resolve('step2')
         })
       }
-      function run_c(data_b) {
-        return new Promise(function (resolve,) {
-          console.log(data_b);
+      function run_c(data_b){
+         return new Promise(function(resolve,){
+           console.log(data_b);
           resolve('step3')
         })
       }
       //continue use it.
-      run_a().then(function (data) {
+      run_a().then(function(data){
         return run_b(data)
-      }).then(function (data) {
+      }).then(function(data){
         return run_c(data)
-      }).then(function (data) {
+      }).then(function(data){
         console.log(data);
       })
     }
@@ -144,7 +144,7 @@ export default {
 button {
   margin-left: 4px;
 }
-.btn {
+.btn{
   vertical-align: center;
   margin: 0 auto;
 }
