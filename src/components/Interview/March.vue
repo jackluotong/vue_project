24787164f19f02086bcc523c331308c75d3c7a0b
LAPTOP-7U2RL3VE\WilliamLuo <jackluotong@outlook.com>
2021-03-22 18:03:00 +0800

    add new interview knowledge.

--- src/components/Interview/March.vue
+++ src/components/Interview/March.vue
@@ -72,7 +72,7 @@ export default {
         { title: '03.01(某内资)', name: '行内元素和块级元素？', answer: '行内元素有span,a,i,b无法设置padding margin/块级元素自成一行p,h,div,li' },
         { name: '伪类和伪元素的区别？', answer: '伪类是比如:after,:hover,逻辑上存在却无需标识/伪元素是虚拟的,比如first,逻辑上存在但是dom中不存在' },
         { name: 'vue中作用域slot', answer: '' },
-        { name: 'vue中render是做什么的？', answer: '把virtual dom变为true dom' },
+        { name: 'vue中render是做什么的？', answer: 'createElement 函数，这个函数的作用就是生成一个 VNode节点，render 函数得到这个 VNode 节点之后，返回给 Vue.js 的 mount 函数，渲染成真实 DOM 节点，并挂载到根节点上。' },
         { name: 'webpack中的loader和plugin的区别？/dependencies和devDependencies区别？', answer: '' },
         { name: 'vue双向绑定详解？m<=>v', answer: 'view=>model用事件监听;model=>view,observer(劫持并监听所有属性，如果有变动的，就通知订阅者。)+watcher(可以收到属性的变化通知并执行相应的函数，从而更新视图)+compile(可以扫描和解析每个节点的相关指令，并根据初始化模板数据以及初始化相应的订阅器。)' },
         { name: 'cros需要做那些配置？', answer: '服务端设置 Access-Control-Allow-Origin 就可以开启 CORS' },
@@ -81,7 +81,7 @@ export default {
         { title: '03.02律商联讯(外资)', name: 'css样式优先级？', answer: '!important > 行内样式>ID选择器 > 类选择器 > 标签 > 通配符 > 继承 > 浏览器默认属性' },
         { name: 'es5继承？', answer: 'prototype实现，组合/寄生/' },
         { name: 'js中的宏任务和微任务？', answer: 'marcotask——ui rendering i/o setTimeout setInterval  /microtask-promise process.nextTick' },
-        { name: 'expert和expert default的区别？', answer: '' },
+        { name: 'expert和expert default的区别？', answer: 'expert 可以导出多个,ex' },
         { name: 'es6新特性？', answer: '' },
         { name: '数据库索引原理？索引加在哪里？sql怎么优化查询？', answer: '' },
         { title: '03.04美团面试', name: 'package-lock.json是做什么的？需要上传到git嘛？', answer: '是用来记录安装包的版本，需要上传' },
@@ -93,7 +93,7 @@ export default {
         { name: '为什么要用node作为中间件？', answer: '1.在工程上的应用：统一了标准，引入了组件化、自动化；2.在服务端的应用：ssr,serverless，多端适配' },
         { name: 'Vue中的组件', answer: '组件注册，传值' },
         { name: '事件循环？', answer: '执行栈和事件队列' },
-        { title: '03.09面试', name: '首屏渲染速度怎么加快？', answer: 'critical rendering path domtree,cssom' },
+        { title: '03.09面试', name: '首屏渲染速度怎么加快？', answer: 'critical rendering path domtree,cssom,关键路径渲染优化,预加载扫描器' },
         { name: '算法排序？以及时间空间复杂度？', answer: '' },//git config http.sslVerify "false" 当无法git pull的时候
         { name: '浏览器渲染页面的原理？', answer: '' },
         { name: '如何确保tcp传输稳定可靠？', answer: '序列号、超时重传、检验和、流量控制、滑动窗口、拥塞控制实现可靠性' },
@@ -118,7 +118,15 @@ export default {
         { name: ' 数组降维？怎么有更高的复用性兼容性？', answer: 'flat,' },
         { name: ' vue中的diff是怎样的？父节点改变的话会影响子节点嘛？', answer: 'flat,' },
         {title:'03.19新致软件',name:'微服务是什么？多页面的通讯怎么实现？',answer:'postMessage'},
-        
+        {title:'03.20东方财富',name:'router和route的区别？',answer:'router是vue-router的对象全局的路由对象可以用作push等操作/route是当前正在跳转的对象用作获取name,path,params,query'},
+        { name: ' 节流防抖的原理', answer: ',' },
+        { title: '03.22 中银金融面试', name: 'null和undefined的区别？', answer: ''},
+        { name: ' v-if和v-show的区别？', answer: ',' },
+        { name: ' vue父组件和子组件的生命周期执行顺序？', answer: ',' },
+        { name: ' promise构造函数是同步执行还是异步？then？', answer: ',' },
+        { name: ' const定义的变量可以更改嘛？', answer: '数组可以更改,直接定义的常量无法修改' },
+        { name: ' map,set数据结构？', answer: ',' },
+
 
       ],
       img: require('../../assets/mvvm.png')//做图片动态绑定的时候需要用require
