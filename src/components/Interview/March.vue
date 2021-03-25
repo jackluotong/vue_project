dc62a3d7d10583e6e30443d6e8359f0369b36e71
jackluotong <jackluotong@outlook.com>
2021-03-25 10:26:31 +0800

    test vuex part.

--- src/components/Interview/March.vue
+++ src/components/Interview/March.vue
@@ -1,236 +1,399 @@
-
 <template>
-  <div style="background:#ECECEC; padding:20px;">
-    <!-- interview part from march -->
-    <a-card
-      title="2021-03 Interview"
-      :bordered="false"
-      style="width:98%;margin:0 auto"
-    >
-      <div class="content">
-        <ol>
-          <li
-            v-for="(item,index,id) in interview"
-            :key='id'
-            class="liClass"
-          >
-            {{item.index}}{{item.title}} <span>题目：</span> {{item.name}}<br>答案：<span style="color:blue">{{item.answer}}. </span>
-          </li>
-        </ol>
-        <!-- <img :src='img'/> -->
-      </div>
-    </a-card>
+	<div style="background:#ECECEC; padding:20px;">
+		<!-- interview part from march -->
+		<a-card
+			title="2021-03 Interview"
+			:bordered="false"
+			style="width:98%;margin:0 auto"
+		>
+			<div class="content">
+				<ol>
+					<li
+						v-for="(item, index, id) in interview"
+						:key="id"
+						class="liClass"
+					>
+						{{ item.index }}{{ item.title }} <span>题目：</span>
+						{{ item.name }}<br />答案：<span style="color:blue"
+							>{{ item.answer }}.
+						</span>
+					</li>
+				</ol>
+				<!-- <img :src='img'/> -->
+			</div>
+		</a-card>
 
-    <!-- click function -->
-    <a-card
-      title="2021-03 Interview"
-      :bordered="false"
-      style="width:98%;margin:0 auto"
-    >
-      <div class="content">
-        <a-button
-          type='primary'
-          @click='promiseFun'
-          class="btn"
-        >promise test</a-button>
-        <a-button
-          type='primary'
-          @click='promiseChain'
-          class="btn"
-        >promise chain use</a-button>
-        <a-button
-          type='primary'
-          @click='prototypeFun'
-          class="btn"
-        >prototype</a-button>
-      </div>
-    </a-card>
-    <!-- photo and other -->
-    <a-card
-      title="2021-03 Interview"
-      :bordered="false"
-      style="width:98%;margin:0 auto"
-    >
-      <div class="content">
-        <img :src='img' />
-        <!-- 测试父子组件传值 -->
-        <div class="train-city">
-          <h1>父组件传给子组件的:{{sendData}}</h1>
-          <br/> <a-button @click="select('shanghai')">点击此处将‘上海发给父组件’</a-button>
-        </div>
-      </div>
-    </a-card>
-    <div>
-      <div style="height:100px;padding:20px;margin:15px;background:red">1</div>
-      <div style="height:50px;padding:10px;margin:5px;background:blue">2</div>
-
-    </div>
-  </div>
-
+		<!-- click function -->
+		<a-card
+			title="2021-03 Interview"
+			:bordered="false"
+			style="width:98%;margin:0 auto"
+		>
+			<div class="content">
+				<a-button type="primary" @click="promiseFun" class="btn"
+					>promise test</a-button
+				>
+				<a-button type="primary" @click="promiseChain" class="btn"
+					>promise chain use</a-button
+				>
+				<a-button type="primary" @click="prototypeFun" class="btn"
+					>prototype</a-button
+				>
+			</div>
+		</a-card>
+		<!-- photo and other -->
+		<a-card
+			title="2021-03 Interview"
+			:bordered="false"
+			style="width:98%;margin:0 auto"
+		>
+			<div class="content">
+				<img :src="img" />
+				<!-- 测试父子组件传值 -->
+				<div class="train-city">
+					<h1>父组件传给子组件的:{{ sendData }}</h1>
+					<br />
+					<a-button @click="select('shanghai')"
+						>点击此处将'上海'发给父组件
+					</a-button>
+				</div>
+			</div>
+		</a-card>
+		<div>
+			<div style="height:100px;padding:20px;margin:15px;background:red">
+				1
+			</div>
+			<div style="height:50px;padding:10px;margin:5px;background:blue">
+				2
+			</div>
+		</div>
+	</div>
 </template>
 <script>
-
 export default {
-  name:'trainCity',
-  props:['sendData'],
-  data() {
-    return {
-      interview: [
-        { title: '03.01(某内资)', name: '行内元素和块级元素？', answer: '行内元素有span,a,i,b无法设置padding margin/块级元素自成一行p,h,div,li' },
-        { name: '伪类和伪元素的区别？', answer: '伪类是比如:after,:hover,逻辑上存在却无需标识/伪元素是虚拟的,比如first,逻辑上存在但是dom中不存在' },
-        { name: 'vue中作用域slot', answer: '' },
-        { name: 'vue中render是做什么的？', answer: 'createElement 函数，这个函数的作用就是生成一个 VNode节点，render 函数得到这个 VNode 节点之后，返回给 Vue.js 的 mount 函数，渲染成真实 DOM 节点，并挂载到根节点上。' },
-        { name: 'webpack中的loader和plugin的区别？/dependencies和devDependencies区别？', answer: '' },
-        { name: 'vue双向绑定详解？m<=>v', answer: 'view=>model用事件监听;model=>view,observer(劫持并监听所有属性，如果有变动的，就通知订阅者。)+watcher(可以收到属性的变化通知并执行相应的函数，从而更新视图)+compile(可以扫描和解析每个节点的相关指令，并根据初始化模板数据以及初始化相应的订阅器。)' },
-        { name: 'cros需要做那些配置？', answer: '服务端设置 Access-Control-Allow-Origin 就可以开启 CORS' },
-        { name: 'promise原理以及all的时候需要传什么参数？', answer: 'promise使用回调函数，把回调函数封装在内部；all的时候把所有的promise放在数组中入参' },
-        { name: '垂直水平居中的方式？', answer: '第一种父元素相对布局，子元素绝对布局+外边距，第二种flex布局，第三种父元素相对布局,子元素绝对布局+transform，第四种 表格' },
-        { title: '03.02律商联讯(外资)', name: 'css样式优先级？', answer: '!important > 行内样式>ID选择器 > 类选择器 > 标签 > 通配符 > 继承 > 浏览器默认属性' },
-        { name: 'es5继承？', answer: 'prototype实现，组合/寄生/' },
-        { name: 'js中的宏任务和微任务？', answer: 'marcotask——ui rendering i/o setTimeout setInterval  /microtask-promise process.nextTick' },
-        { name: 'expert和expert default的区别？', answer: 'expert 可以导出多个,ex' },
-        { name: 'es6新特性？', answer: '' },
-        { name: '数据库索引原理？索引加在哪里？sql怎么优化查询？', answer: '' },
-        { title: '03.04美团面试', name: 'package-lock.json是做什么的？需要上传到git嘛？', answer: '是用来记录安装包的版本，需要上传' },
-        { name: 'cdn和dns有什么区别？', answer: 'cdn:内容分发网络,让用户能够更快速的得到请求的数据/dns:将域名转换为ip地址' },
-        { name: 'css垂直居中和水平垂直居中？', answer: '' },
-        { name: '数组的降维？', answer: 'arr+= " "arr=arr.split(","),flat,递归,stack,reduce,concat' },
-        { name: 'prototype怎么应用？', answer: '做一些继承,给对象里添加属性和方法' },
-        { name: 'http常见状态码？1 2 3 4 5 ', answer: '1代表信息类；2代表成功类；3代表重定向类；4代表客户端错误；5代表服务端错误' },
-        { name: '为什么要用node作为中间件？', answer: '1.在工程上的应用：统一了标准，引入了组件化、自动化；2.在服务端的应用：ssr,serverless，多端适配' },
-        { name: 'Vue中的组件', answer: '组件注册，传值' },
-        { name: '事件循环？', answer: '执行栈和事件队列' },
-        { title: '03.09面试', name: '首屏渲染速度怎么加快？', answer: 'critical rendering path domtree,cssom,关键路径渲染优化,预加载扫描器' },
-        { name: '算法排序？以及时间空间复杂度？', answer: '' },//git config http.sslVerify "false" 当无法git pull的时候
-        { name: '浏览器渲染页面的原理？', answer: '' },
-        { name: '如何确保tcp传输稳定可靠？', answer: '序列号、超时重传、检验和、流量控制、滑动窗口、拥塞控制实现可靠性' },
-        { name: '从输入url到页面展现的过程？', answer: '' },
-        { name: '防抖节流怎么实现？', answer: '' },
-        { name: '构造函数和prototype的区别？', answer: '构造函数存在浪费内存' },
-        { name: 'webpack打包原理？', answer: '把依赖的模块转化成可以代表这些包的静态文件，分析代码。转换代码，编译代码，输出代码' },
-        { title: '03.16面试', name: 'vue中的render是做什么的？', answer: 'createElement，转换为virtual dom' },
-        { name: '网络安全问题有那些？', answer: 'xss(跨站脚本攻击，由于对用户提交的数据没有经过严格的过滤处理造成的，解决：XSS Filter，Http Only cookie) xsrf(跨站请求伪造，a网站的cookie被b网站使用；解决方案：使用token在发送http请求的时候) sql注入，xxe漏洞，' },
-        { name: '深拷贝和浅拷贝的区别？', answer: '' },//github.com.cnpmjs.org 在com后面增加这个配置来加速clone
-        { name: 'tcp和udp的区别？', answer: 'tcp是面向连接，稳定，安全可靠，无差错，无重复，速度会慢一点；udp是无面向连接，' },
-        { name: 'let const var的区别？', answer: '' },
-        { name: '栈 队列 堆的区别？', answer: '' },
-        { name: '构造函数与prototype 原型链？', answer: '' },
-        { title: '03.17阿里旗下二面', name: 'vue3的原理？以及新特性？', answer: ''},
-        { name: ' 数组降维？怎么有更高的复用性兼容性？', answer: '' },
-        { name: ' react与vue的区别？', answer: 'Vue提供反应式的数据，当数据改动时，界面就会自动更新，而React里面需要调用方法SetState' },   
-        { name: ' kepp-alive的原理以及生命周期？', answer: '页面第一次进入的时候，钩子触发的顺序是created->mounted->activated' },  
-        { name: ' vue中的mixin？', answer: '多个组件可以共享数据和方法，在使用mixin的组件中引入后，mixin中的方法和属性也就并入到该组件中，可以直接使用。钩子函数会两个都被调用，mixin中的钩子首先执行。' },   
-        { name: ' 数组降维？怎么有更高的复用性兼容性？', answer: 'flat,' },
-        { title: '03.18华测导航面试', name: '浏览器渲染页面的原理？渲染过程以及如何阻止js阻塞？', answer: 'domtree =>cssom=>render tree'},
-        { name: ' 数组降维？怎么有更高的复用性兼容性？', answer: 'flat,' },
-        { name: ' vue中的diff是怎样的？父节点改变的话会影响子节点嘛？', answer: 'flat,' },
-        {title:'03.19新致软件',name:'微服务是什么？多页面的通讯怎么实现？',answer:'postMessage'},
-        {title:'03.20东方财富',name:'router和route的区别？',answer:'router是vue-router的对象全局的路由对象可以用作push等操作/route是当前正在跳转的对象用作获取name,path,params,query'},
-        { name: ' 节流防抖的原理', answer: ',' },
-        { title: '03.22 中银金融面试', name: 'null和undefined的区别？', answer: ''},
-        { name: ' v-if和v-show的区别？', answer: ',' },
-        { name: ' vue父组件和子组件的生命周期执行顺序？', answer: ',' },
-        { name: ' promise构造函数是同步执行还是异步？then？', answer: ',' },
-        { name: ' const定义的变量可以更改嘛？', answer: '数组可以更改,直接定义的常量无法修改' },
-        { name: ' map,set数据结构？', answer: ',' },
-        { title: '03.22 印度面试', name: '', answer: ''},
-        { name: ' absolute和relative的区别？', answer: ',' },
-        { name: ' react中的hooks？', answer: ',' },
-        { name: ' let作用域问题？', answer: ',' },
-        { title: '03.23 医疗面试', name: '值类型和引用类型的区别？', answer: ''},
-        { name: '普通函数和箭头函数的区别？', answer: ',' },
-        { name: ' 闭包的作用和使用方法？', answer: ',' },
-        { name: 'js中全局变量的存储', answer: '全局变量全部存放在静态存储区，在程序开始执行时给全局变量分配存储区，程序行完毕就释放。在程序执行过程中它们占据固定的存储单元，而不动态地进行分配和释放,' },//alt+z=换行
-        { name: 'vuex中的dispatch和commit', answer: ',' },
-        { title: '03.24 汉得面试', name: '值类型和引用类型的区别？', answer: ''},
-        { name: '浏览器的事件流？', answer: ',' },
-        { name: '为什么要用事件委托？', answer: ',' },
-        { name: '什么是回流？重绘？', answer: ',' },
-        { name: ' 怎么实现0.5px的线？', answer: ',' },
-        { name: 'flex布局？以及其中的各种属性？', answer: ',' },
-        { name: '闭包的优缺点？', answer: ',' },
-        { name: 'react中怎么提升渲染效果？', answer: ',' },
-        { name: 'react和vue的区别？', answer: ',' },
-      ],
-      img: require('../../assets/mvvm.png')//做图片动态绑定的时候需要用require
-    }
-  },
-  
-  methods: {
-    select(val){
-      let data={
-        cityName:val
-      }
-      this.$emit('showCityName',data)
-    },
-    promiseFun: function () {
-      let myMap=new Map()
-      myMap.set('1','1111')
-      alert(myMap)
-      let run = function () {
-        let _promise = new Promise(function (resolve, reject) {
-          setTimeout(() => {
-            let rand = Math.random()
-            if (rand > 0.5) {
-              resolve('resolve' + rand)
-            } else {
-              reject('reject' + rand)
-            }
-          }, 1000);
-        })
-        return _promise
-      }
-      run()
-    },
-    promiseChain: function () {
-      function run_a() {
-        return new Promise(function (resolve,) {
-          resolve('step1')
-        })
-      }
-      function run_b(data_a) {
-        return new Promise(function (resolve,) {
-          console.log(data_a);
-          resolve('step2')
-        })
-      }
-      function run_c(data_b) {
-        return new Promise(function (resolve,) {
-          console.log(data_b);
-          resolve('step3')
-        })
-      }
-      //continue use it.
-      run_a().then(function (data) {
-        return run_b(data)
-      }).then(function (data) {
-        return run_c(data)
-      }).then(function (data) {
-        console.log(data);
-      })
-    },
-    prototypeFun:function () {
-      function a(name){
-        alert(name)
-      }
-      a('jack')
-      //
-    }
-  },
+	name: 'trainCity',
+	props: ['sendData'],
+	data() {
+		return {
+			interview: [
+				{
+					title: '03.01(某内资)',
+					name: '行内元素和块级元素？',
+					answer:
+						'行内元素有span,a,i,b无法设置padding margin/块级元素自成一行p,h,div,li',
+				},
+				{
+					name: '伪类和伪元素的区别？',
+					answer:
+						'伪类是比如:after,:hover,逻辑上存在却无需标识/伪元素是虚拟的,比如first,逻辑上存在但是dom中不存在',
+				},
+				{ name: 'vue中作用域slot', answer: '' },
+				{
+					name: 'vue中render是做什么的？',
+					answer:
+						'createElement 函数，这个函数的作用就是生成一个 VNode节点，render 函数得到这个 VNode 节点之后，返回给 Vue.js 的 mount 函数，渲染成真实 DOM 节点，并挂载到根节点上。',
+				},
+				{
+					name:
+						'webpack中的loader和plugin的区别？/dependencies和devDependencies区别？',
+					answer: '',
+				},
+				{
+					name: 'vue双向绑定详解？m<=>v',
+					answer:
+						'view=>model用事件监听;model=>view,observer(劫持并监听所有属性，如果有变动的，就通知订阅者。)+watcher(可以收到属性的变化通知并执行相应的函数，从而更新视图)+compile(可以扫描和解析每个节点的相关指令，并根据初始化模板数据以及初始化相应的订阅器。)',
+				},
+				{
+					name: 'cros需要做那些配置？',
+					answer:
+						'服务端设置 Access-Control-Allow-Origin 就可以开启 CORS',
+				},
+				{
+					name: 'promise原理以及all的时候需要传什么参数？',
+					answer:
+						'promise使用回调函数，把回调函数封装在内部；all的时候把所有的promise放在数组中入参',
+				},
+				{
+					name: '垂直水平居中的方式？',
+					answer:
+						'第一种父元素相对布局，子元素绝对布局+外边距，第二种flex布局，第三种父元素相对布局,子元素绝对布局+transform，第四种 表格',
+				},
+				{
+					title: '03.02律商联讯(外资)',
+					name: 'css样式优先级？',
+					answer:
+						'!important > 行内样式>ID选择器 > 类选择器 > 标签 > 通配符 > 继承 > 浏览器默认属性',
+				},
+				{ name: 'es5继承？', answer: 'prototype实现，组合/寄生/' },
+				{
+					name: 'js中的宏任务和微任务？',
+					answer:
+						'marcotask——ui rendering i/o setTimeout setInterval  /microtask-promise process.nextTick',
+				},
+				{
+					name: 'expert和expert default的区别？',
+					answer: 'expert 可以导出多个,ex',
+				},
+				{ name: 'es6新特性？', answer: '' },
+				{
+					name: '数据库索引原理？索引加在哪里？sql怎么优化查询？',
+					answer: '',
+				},
+				{
+					title: '03.04美团面试',
+					name: 'package-lock.json是做什么的？需要上传到git嘛？',
+					answer: '是用来记录安装包的版本，需要上传',
+				},
+				{
+					name: 'cdn和dns有什么区别？',
+					answer:
+						'cdn:内容分发网络,让用户能够更快速的得到请求的数据/dns:将域名转换为ip地址',
+				},
+				{ name: 'css垂直居中和水平垂直居中？', answer: '' },
+				{
+					name: '数组的降维？',
+					answer:
+						'arr+= " "arr=arr.split(","),flat,递归,stack,reduce,concat',
+				},
+				{
+					name: 'prototype怎么应用？',
+					answer: '做一些继承,给对象里添加属性和方法',
+				},
+				{
+					name: 'http常见状态码？1 2 3 4 5 ',
+					answer:
+						'1代表信息类；2代表成功类；3代表重定向类；4代表客户端错误；5代表服务端错误',
+				},
+				{
+					name: '为什么要用node作为中间件？',
+					answer:
+						'1.在工程上的应用：统一了标准，引入了组件化、自动化；2.在服务端的应用：ssr,serverless，多端适配',
+				},
+				{ name: 'Vue中的组件', answer: '组件注册，传值' },
+				{ name: '事件循环？', answer: '执行栈和事件队列' },
+				{
+					title: '03.09面试',
+					name: '首屏渲染速度怎么加快？',
+					answer:
+						'critical rendering path domtree,cssom,关键路径渲染优化,预加载扫描器',
+				},
+				{ name: '算法排序？以及时间空间复杂度？', answer: '' }, //git config http.sslVerify "false" 当无法git pull的时候
+				{ name: '浏览器渲染页面的原理？', answer: '' },
+				{
+					name: '如何确保tcp传输稳定可靠？',
+					answer:
+						'序列号、超时重传、检验和、流量控制、滑动窗口、拥塞控制实现可靠性',
+				},
+				{ name: '从输入url到页面展现的过程？', answer: '' },
+				{ name: '防抖节流怎么实现？', answer: '' },
+				{
+					name: '构造函数和prototype的区别？',
+					answer: '构造函数存在浪费内存',
+				},
+				{
+					name: 'webpack打包原理？',
+					answer:
+						'把依赖的模块转化成可以代表这些包的静态文件，分析代码。转换代码，编译代码，输出代码',
+				},
+				{
+					title: '03.16面试',
+					name: 'vue中的render是做什么的？',
+					answer: 'createElement，转换为virtual dom',
+				},
+				{
+					name: '网络安全问题有那些？',
+					answer:
+						'xss(跨站脚本攻击，由于对用户提交的数据没有经过严格的过滤处理造成的，解决：XSS Filter，Http Only cookie) xsrf(跨站请求伪造，a网站的cookie被b网站使用；解决方案：使用token在发送http请求的时候) sql注入，xxe漏洞，',
+				},
+				{ name: '深拷贝和浅拷贝的区别？', answer: '' }, //github.com.cnpmjs.org 在com后面增加这个配置来加速clone
+				{
+					name: 'tcp和udp的区别？',
+					answer:
+						'tcp是面向连接，稳定，安全可靠，无差错，无重复，速度会慢一点；udp是无面向连接，',
+				},
+				{ name: 'let const var的区别？', answer: '' },
+				{ name: '栈 队列 堆的区别？', answer: '' },
+				{ name: '构造函数与prototype 原型链？', answer: '' },
+				{
+					title: '03.17阿里旗下二面',
+					name: 'vue3的原理？以及新特性？',
+					answer: '',
+				},
+				{ name: ' 数组降维？怎么有更高的复用性兼容性？', answer: '' },
+				{
+					name: ' react与vue的区别？',
+					answer:
+						'Vue提供反应式的数据，当数据改动时，界面就会自动更新，而React里面需要调用方法SetState',
+				},
+				{
+					name: ' kepp-alive的原理以及生命周期？',
+					answer:
+						'页面第一次进入的时候，钩子触发的顺序是created->mounted->activated',
+				},
+				{
+					name: ' vue中的mixin？',
+					answer:
+						'多个组件可以共享数据和方法，在使用mixin的组件中引入后，mixin中的方法和属性也就并入到该组件中，可以直接使用。钩子函数会两个都被调用，mixin中的钩子首先执行。',
+				},
+				{
+					name: ' 数组降维？怎么有更高的复用性兼容性？',
+					answer: 'flat,',
+				},
+				{
+					title: '03.18华测导航面试',
+					name: '浏览器渲染页面的原理？渲染过程以及如何阻止js阻塞？',
+					answer: 'domtree =>cssom=>render tree',
+				},
+				{
+					name: ' 数组降维？怎么有更高的复用性兼容性？',
+					answer: 'flat,',
+				},
+				{
+					name:
+						' vue中的diff是怎样的？父节点改变的话会影响子节点嘛？',
+					answer: 'flat,',
+				},
+				{
+					title: '03.19新致软件',
+					name: '微服务是什么？多页面的通讯怎么实现？',
+					answer: 'postMessage',
+				},
+				{
+					title: '03.20东方财富',
+					name: 'router和route的区别？',
+					answer:
+						'router是vue-router的对象全局的路由对象可以用作push等操作/route是当前正在跳转的对象用作获取name,path,params,query',
+				},
+				{ name: ' 节流防抖的原理', answer: ',' },
+				{
+					title: '03.22 中银金融面试',
+					name: 'null和undefined的区别？',
+					answer: '',
+				},
+				{ name: ' v-if和v-show的区别？', answer: ',' },
+				{ name: ' vue父组件和子组件的生命周期执行顺序？', answer: ',' },
+				{
+					name: ' promise构造函数是同步执行还是异步？then？',
+					answer: ',',
+				},
+				{
+					name: ' const定义的变量可以更改嘛？',
+					answer: '数组可以更改,直接定义的常量无法修改',
+				},
+				{ name: ' map,set数据结构？', answer: ',' },
+				{ title: '03.22 印度面试', name: '', answer: '' },
+				{ name: ' absolute和relative的区别？', answer: ',' },
+				{ name: ' react中的hooks？', answer: ',' },
+				{ name: ' let作用域问题？', answer: ',' },
+				{
+					title: '03.23 医疗面试',
+					name: '值类型和引用类型的区别？',
+					answer: '',
+				},
+				{ name: '普通函数和箭头函数的区别？', answer: ',' },
+				{ name: ' 闭包的作用和使用方法？', answer: ',' },
+				{
+					name: 'js中全局变量的存储',
+					answer:
+						'全局变量全部存放在静态存储区，在程序开始执行时给全局变量分配存储区，程序行完毕就释放。在程序执行过程中它们占据固定的存储单元，而不动态地进行分配和释放,',
+				}, //alt+z=换行
+				{ name: 'vuex中的dispatch和commit', answer: ',' },
+				{
+					title: '03.24 汉得面试',
+					name: '值类型和引用类型的区别？',
+					answer: '',
+				},
+				{ name: '浏览器的事件流？', answer: ',' },
+				{ name: '为什么要用事件委托？', answer: ',' },
+				{ name: '什么是回流？重绘？', answer: ',' },
+				{ name: ' 怎么实现0.5px的线？', answer: ',' },
+				{ name: 'flex布局？以及其中的各种属性？', answer: ',' },
+				{ name: '闭包的优缺点？', answer: ',' },
+				{ name: 'react中怎么提升渲染效果？', answer: ',' },
+				{ name: 'react和vue的区别？', answer: ',' },
+			],
+			img: require('../../assets/mvvm.png'), //做图片动态绑定的时候需要用require
+		}
+	},
+
+	methods: {
+		select(val) {
+			let data = {
+				cityName: val,
+			}
+			this.$emit('showCityName', data)
+		},
+		promiseFun: function() {
+			let myMap = new Map()
+			myMap.set('1', '1111')
+			alert(myMap)
+			let run = function() {
+				let _promise = new Promise(function(resolve, reject) {
+					setTimeout(() => {
+						let rand = Math.random()
+						if (rand > 0.5) {
+							resolve('resolve' + rand)
+						} else {
+							reject('reject' + rand)
+						}
+					}, 1000)
+				})
+				return _promise
+			}
+			run()
+		},
+		promiseChain: function() {
+			function run_a() {
+				return new Promise(function(resolve) {
+					resolve('step1')
+				})
+			}
+			function run_b(data_a) {
+				return new Promise(function(resolve) {
+					console.log(data_a)
+					resolve('step2')
+				})
+			}
+			function run_c(data_b) {
+				return new Promise(function(resolve) {
+					console.log(data_b)
+					resolve('step3')
+				})
+			}
+			//continue use it.
+			run_a()
+				.then(function(data) {
+					return run_b(data)
+				})
+				.then(function(data) {
+					return run_c(data)
+				})
+				.then(function(data) {
+					console.log(data)
+				})
+		},
+		prototypeFun: function() {
+			function a(name) {
+				alert(name)
+			}
+			a('jack')
+			//
+		},
+	},
 }
 </script>
 <style scoped>
 .content {
-  float: left;
+	float: left;
 }
 .liClass {
-  text-align: left;
+	text-align: left;
 }
 button {
-  margin-left: 4px;
+	margin-left: 4px;
 }
 .btn {
-  vertical-align: center;
-  margin: 0 auto;
+	vertical-align: center;
+	margin: 0 auto;
 }
 </style>
--- src/components/Protocol/Index.vue
+++ src/components/Protocol/Index.vue
@@ -1,20 +1,20 @@
-
 <template>
   <div style="background:#ECECEC; padding:20px;">
     <!-- all protocol from 2021.03.11 -->
     <a-card
-      title="TCP"
-      :bordered="false"
-      style="width:98%;margin:0 auto"
+        title="TCP"
+        :bordered="false"
+        style="width:98%;margin:0 auto"
     >
       <div class="content">
         <ol>
           <li
-            v-for="(item,index,id) in TCP"
-            :key='id'
-            class="liClass"
+              v-for="(item,index,id) in TCP"
+              :key='id'
+              class="liClass"
           >
-            {{item.index}}{{item.title}} <span>题目：</span> {{item.name}}<br>答案：<span style="color:blue">{{item.answer}}. </span>
+            {{ item.index }}{{ item.title }} <span>题目：</span> {{ item.name }}<br>答案：<span
+              style="color:blue">{{ item.answer }}. </span>
           </li>
         </ol>
       </div>
@@ -22,41 +22,42 @@
 
     <!-- HTTP -->
     <a-card
-      title="HTTP"
-      :bordered="false"
-      style="width:98%;margin:0 auto"
+        title="HTTP"
+        :bordered="false"
+        style="width:98%;margin:0 auto"
     >
       <div class="content">
-         <ol>
+        <ol>
           <li
-            v-for="(item,index,id) in HTTP"
-            :key='id'
-            class="liClass"
+              v-for="(item,index,id) in HTTP"
+              :key='id'
+              class="liClass"
           >
-            {{item.index}}{{item.title}} <span>题目：</span> {{item.name}}<br>答案：<span style="color:blue">{{item.answer}}. </span>
+            {{ item.index }}{{ item.title }} <span>题目：</span> {{ item.name }}<br>答案：<span
+              style="color:blue">{{ item.answer }}. </span>
           </li>
         </ol>
       </div>
     </a-card>
     <!-- UDP -->
     <a-card
-      title="UDP"
-      :bordered="false"
-      style="width:98%;margin:0 auto"
+        title="UDP"
+        :bordered="false"
+        style="width:98%;margin:0 auto"
     >
       <div class="content">
-        <img :src='img' />
+        <img :src='img'/>
       </div>
     </a-card>
 
     <!-- IP -->
-      <a-card
-      title="IP"
-      :bordered="false"
-      style="width:98%;margin:0 auto"
+    <a-card
+        title="IP"
+        :bordered="false"
+        style="width:98%;margin:0 auto"
     >
       <div class="content">
-        <img :src='img' />
+        <img :src='img'/>
       </div>
     </a-card>
   </div>
@@ -66,14 +67,24 @@
 export default {
   data() {
     return {
-      TCP:[],
-      HTTP:[
-        {title:'',name:'http?',answer:'HTTP是超文本传输协议，是互联网上用得最多的网络协议， 所以的www文件都遵守这个标准。HTTP 协议工作于客户端—服务端架构上。浏览器作为 HTTP 客户端通过 URL 向 HTTP 服务端（即 Web 服务器） 发送请求'},
-        {name:'https?',answer:'HTTPS 的全称是 Hyper Text Transfer Protocol over Secure Socket Layer，是以安全为目标的 HTTP 通道， 简单讲是 HTTP 的安全版，即 HTTP 下加入 SSL 层（Secure Sockets Layer 安全套接层），简称为 HTTPS'},
-        {name:'http and https different?',asnwer:'1.https更安全，运行于ssl,ssl运行于tcp,是加了加密和认证机制的http;2.不同的连接方式,端口也不同80,4433.https消耗更多的ccpu和内存资源,3https需要向机构购买证书'}
+      TCP: [],
+      HTTP: [
+        {
+          title: '',
+          name: 'http?',
+          answer: 'HTTP是超文本传输协议，是互联网上用得最多的网络协议， 所以的www文件都遵守这个标准。HTTP 协议工作于客户端—服务端架构上。浏览器作为 HTTP 客户端通过 URL 向 HTTP 服务端（即 Web 服务器） 发送请求'
+        },
+        {
+          name: 'https?',
+          answer: 'HTTPS 的全称是 Hyper Text Transfer Protocol over Secure Socket Layer，是以安全为目标的 HTTP 通道， 简单讲是 HTTP 的安全版，即 HTTP 下加入 SSL 层（Secure Sockets Layer 安全套接层），简称为 HTTPS'
+        },
+        {
+          name: 'http and https different?',
+          asnwer: '1.https更安全，运行于ssl,ssl运行于tcp,是加了加密和认证机制的http;2.不同的连接方式,端口也不同80,4433.https消耗更多的ccpu和内存资源,3https需要向机构购买证书'
+        }
       ],
-      UDP:[],
-      IP:[],
+      UDP: [],
+      IP: [],
     }
   },
 }
