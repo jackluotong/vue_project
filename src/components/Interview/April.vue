a267abe7666d76ad4065a68673c5b1892bb0d188
LAPTOP-7U2RL3VE\WilliamLuo <jackluotong@outlook.com>
2021-04-06 18:02:46 +0800

    add new knowledge for interview.

--- src/components/Interview/April.vue
+++ src/components/Interview/April.vue
@@ -35,12 +35,14 @@ export default {
         {name:'垂直居中？水平居中？',answer:''},
         {name:'promise的原理？',answer:''},
         {name:'事件流？',answer:''},
-        {name:'浏览器的缓存',answer:''},
-        {name:'从url输入到页面呈现？',answer:''},
-        {name:'从url输入到页面呈现？',answer:''},
-        {name:'从url输入到页面呈现？',answer:''},
-        {name:'从url输入到页面呈现？',answer:''},
-
+        {name:'浏览器的缓存',answer:'强缓存协商缓存'},
+        {title:'04.06 X公司面试',name:'js的垃圾回收机制？',answer:''},
+        {name:'什么场景下要规避闭包？',answer:'setTimeout 回调函数'},
+        {name:'为什么const定义的对象可以改变？',answer:'如果是基本数据类型再次修改会报错,如果是object是引用类型的不变的是其指针,但是属性可以改变的'},
+        {name:'垂直居中？水平居中？',answer:''},
+        {name:'rem？rem原理？',answer:''},
+        {name:'v-if v-show的使用场景？以及区别？',answer:''},
+        {name:'微前端？',answer:''},
       ]
     }
   },
