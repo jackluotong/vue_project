edee53b15e3db2349543750474b71205630201ef
LAPTOP-7U2RL3VE\WilliamLuo <jackluotong@outlook.com>
2021-05-12 18:03:35 +0800

    add new.

--- src/ComponentTest/Child.vue
+++ src/ComponentTest/Child.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="SonDiv">
-        <h1>我是子组件的标题</h1>    
-        <p>父组件给子组件传的值：{{title}}</p>
-        <p>{{this.$store.state.count}}</p>
-
+        <h1>我是子组件的标题</h1> 
+        <a-card>
+                      <p>父组件给子组件传的name：{{title}}</p>
+                      <p>父组件给子组件传的name：{{numbers}}</p>
+        </a-card>  
         <hr/>
-        <el-button @click="sendToFather(toFatherValue)">子组件给父组件传参</el-button>
-        <input v-model='toFatherValue' placeholder="请输入要传给父组件的值"/>
+        <a-card>
+                  <el-button @click="sendToFather()">子组件给父组件传参</el-button>
+                  <input v-model='inputDataName' placeholder="请输入要传给父组件的名字"/>
+                  <input v-model='inputDataNum' placeholder="请输入要传给父组件的数字"/>
+                  <a-button @click="test">parent</a-button>
+        </a-card>
     </div>
 </template>
 <script>
@@ -15,23 +20,41 @@ export default {
         title:{
             type:String,
             default:'我是子组件的默认值'
+        },
+        numbers:{
+              type:Number,
+              default:1
         }
     },
     data () {
         return {
             value:this.title,
-            toFatherValue:''
+            inputDataName:'',
+            inputDataNum:'',
+            testChildren:'children'
         }
     },
-    
+    watch:{
+    },
     mounted(){
     },
     methods: {
-        sendToFather(toFatherValue){
+        sendToFather(){
             const data={
-                num:toFatherValue
+                num:this.inputDataNum,
+                name:this.inputDataName
             }
             this.$emit('sendToFather',data)
+        },
+        test:function() {
+            //  this.$pa.value+1
+            // console.log(this.$parent.value)
+            // this.$parent.value=1
+            let a=1
+            this.$parent.testFather(a)
+        },
+        testSon:function(){
+              alert('son component.')
         }
     },
 }
@@ -39,7 +62,6 @@ export default {
 <style scoped>
 .SonDiv{
     background-color: blanchedalmond;
-    height: 400px;
     margin: 10px;
     vertical-align: middle;/**垂直居中 */
     text-align: center;
