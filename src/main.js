7ac131a781a36e5af5d696ad9224391ec8879743
LAPTOP-7U2RL3VE\WilliamLuo <jackluotong@outlook.com>
2021-06-23 19:27:13 +0800

    add

--- src/main.js
+++ src/main.js
@@ -8,12 +8,12 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import axios from 'axios'
 import VueResource from 'vue-resource'
-import iview from 'iview'
+// import iview from 'iview'
 import 'iview/dist/styles/iview.css'
 Vue.prototype.axios = axios
 Vue.use(Antd)
 Vue.use(ElementUI)
-Vue.use(iview)
+// Vue.use(iview)
 Vue.use(VueResource)
 Vue.config.productionTip = false
 
