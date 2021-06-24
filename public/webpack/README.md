a7046a266de4666f67885cb3c4af6a7d3cf572d0
jackluotong@outlook.com <jackluotong@outlook.com>
2021-06-25 00:52:38 +0800

    add webpack learn record.

---
+++ public/webpack/README.md
@@ -0,0 +1,7 @@
+### webpack简介
+    ## 核心概念
+            1.Entry
+            2.Output
+            3.Loader：非js文件
+            4.Plugins：压缩
+            5.Mode：pro env
--- src/components/TestUse/CheckBox.vue
+++ src/components/TestUse/CheckBox.vue
@@ -1,49 +1,37 @@
 <style scoped>
-#myCanvs{
-    border: 1px solid rgb(3, 3, 3);
+#myCanvs {
+  border: 1px solid rgb(3, 3, 3);
 }
 </style>
 <template>
+  <div>
     <div>
-        <div>
-            <h1>
-                防抖的应用（debonuce）
-            </h1>
-            <input type="text" @keyup="keyUpClick" />
-        </div>
-        <div>
-            <h1>
-                截流的应用（throttle）
-            </h1>
-        </div>
-        <div>
-            <p
-                ref="testRef"
-                id="test01"
-                v-show="checkAllGroup.indexOf('西瓜') > -1"
-            >
-                11112
-            </p>
-        </div>
-        <user
-            :usertick="userName"
-            :title="title"
-            :columns="columns"
-            :confData="confData"
-        />
-        <span v-if="0 > 1">
-            {{ nameComputed }}
-        </span>
-<<<<<<< HEAD
-        <div>
-            <canvas id='myCanvs' width="800" height="800">
-            </canvas>
-        </div>
-=======
-        <a-button @click="push">push array</a-button>
-        <h1 ref="domRef">nameFor</h1>
->>>>>>> 88e977dbba561d36e99716885c197f09dc2310be
+      <h1>防抖的应用（debonuce）</h1>
+      <input type="text" @keyup="keyUpClick" />
     </div>
+    <div>
+      <h1>截流的应用（throttle）</h1>
+    </div>
+    <div>
+      <p ref="testRef" id="test01" v-show="checkAllGroup.indexOf('西瓜') > -1">
+        11112
+      </p>
+    </div>
+    <user
+      :usertick="userName"
+      :title="title"
+      :columns="columns"
+      :confData="confData"
+    />
+    <span v-if="0 > 1">
+      {{ nameComputed }}
+    </span>
+    <div>
+      <canvas id="myCanvs" width="800" height="800"> </canvas>
+    </div>
+    <a-button @click="push">push array</a-button>
+    <h1 ref="domRef">nameFor</h1>
+  </div>
 </template>
 <script>
 import { mapState } from 'vuex'
@@ -53,124 +41,122 @@ import User from '../ForPackage/Test'
     debounce 防抖 多次提交只执行一次 按钮提交 搜索框联想场景  防抖1秒 连续点按钮10秒，在11秒的时候，执行一次
     throttle 截流  节流是每隔固定时间执行一次  节流1秒 连续点按钮10秒，每秒执行一次，共执行10次
 */
-const throttle = (function() {
-    let timer = 0
-    return function(callback, time) {
-        clearTimeout(timer)
-        timer = setTimeout(callback, time)
-    }
+const throttle = (function () {
+  let timer = 0
+  return function (callback, time) {
+    clearTimeout(timer)
+    timer = setTimeout(callback, time)
+  }
 })()
 export default {
-    // name: 'search',
-    components: {
-        User,
-    },
-    data() {
-        return {
-            nameFor: 'nameFor',
-            computed: [1, 2, 3, 4],
-            searchValue: '',
-            indeterminate: true,
-            checkAll: false,
-            checkAllGroup: ['香蕉', '西瓜'],
-            columns: [
-                {
-                    title: '角色名称',
-                    key: 'roleName',
-                    tooltip: true,
-                    width: 300,
-                    align: 'center',
-                },
-                {
-                    title: '角色code',
-                    key: 'roleCode',
-                    width: 300,
-                    align: 'center',
-                },
-                {
-                    title: '操作',
-                    slot: 'action',
-                    align: 'center',
-                },
-            ],
-<<<<<<< HEAD
-            confData: [
-            { roleName: 'jack', roleCode: 'luo', id: '001' },
-            { roleName: 'jack', roleCode: 'li', id: '002' },
-            { roleName: 'jack', roleCode: 'wang', id: '003' },
-            { roleName: 'jack', roleCode: 'shi', id: '004' }],
-=======
-            myTestObject: {
-                sex: 'boy',
-                name: 'luo',
-                age: 26,
-                child: { name: 'samll luo', age: 0, sex: 'boy' },
-            },
-            confData: [{ roleName: 'jack', roleCode: 'luo' }],
->>>>>>> 88e977dbba561d36e99716885c197f09dc2310be
-        }
-    },
-    methods: {
-        keyUpClick() {},
-        push() {
-            this.computed.push('0624')
+  // name: 'search',
+  components: {
+    User,
+  },
+  data() {
+    return {
+      nameFor: 'nameFor',
+      computed: [1, 2, 3, 4],
+      searchValue: '',
+      indeterminate: true,
+      checkAll: false,
+      checkAllGroup: ['香蕉', '西瓜'],
+      columns: [
+        {
+          title: '角色名称',
+          key: 'roleName',
+          tooltip: true,
+          width: 300,
+          align: 'center',
         },
+        {
+          title: '角色code',
+          key: 'roleCode',
+          width: 300,
+          align: 'center',
+        },
+        {
+          title: '操作',
+          slot: 'action',
+          align: 'center',
+        },
+      ],
+      confData: [
+        { roleName: 'jack', roleCode: 'luo', id: '001' },
+        { roleName: 'jack', roleCode: 'li', id: '002' },
+        { roleName: 'jack', roleCode: 'wang', id: '003' },
+        { roleName: 'jack', roleCode: 'shi', id: '004' },
+      ],
+      myTestObject: {
+        sex: 'boy',
+        name: 'luo',
+        age: 26,
+        child: { name: 'samll luo', age: 0, sex: 'boy' },
+      },
+      confData: [{ roleName: 'jack', roleCode: 'luo' }],
+    }
+  },
+  methods: {
+    keyUpClick() {},
+    push() {
+      this.computed.push('0624')
     },
-    mounted() {
-        window.my = this
-        /*  this.$nextTick(() => {
+  },
+  mounted() {
+    window.my = this
+    /*  this.$nextTick(() => {
             this.nameFor = 'created'
         }) */
+  },
+  watch: {
+    computed(value) {
+      //can not watch array function not include in vue
+      console.log(value)
     },
-    watch: {
-        computed(value) {
-            //can not watch array function not include in vue
-            console.log(value)
-        },
-        columns: {
-            handler(value) {
-                console.log(value)
-            },
-            deep: true,
-        },
-        'myTestObject.child'(value) {
-            console.log(value)
-            console.log(this.myTestObject)
-        },
-        myTestObject: {
-            handler(value) {
-                console.log(value)
-            },
-            deep: true,
-        },
+    columns: {
+      handler(value) {
+        console.log(value)
+      },
+      deep: true,
     },
-    computed: {
-        userName() {
-            return 'jack+父组件给的值'
-        },
-        nameComputed() {
-            return this.computed.includes(1) === true ? 1 : this.computed
-        },
-        title() {
-            return 'jack luo'
-        },
+    'myTestObject.child'(value) {
+      console.log(value)
+      console.log(this.myTestObject)
     },
-    created() {
-        /* this.axios
+    myTestObject: {
+      handler(value) {
+        console.log(value)
+      },
+      deep: true,
+    },
+  },
+  computed: {
+    userName() {
+      return 'jack+父组件给的值'
+    },
+    nameComputed() {
+      return this.computed.includes(1) === true ? 1 : this.computed
+    },
+    title() {
+      return 'jack luo'
+    },
+  },
+  created() {
+    /* this.axios
             .get(' http://yang.cn1.utools.club/dcenter/application/queryAppAll')
             .then((res) => {
                 console.log(res)
             }) */
-        /*   console.log(
+    /*   console.log(
             this.cityList.forEach((item, id, x) => {
                 // 当前值 索引 总共
                 console.log(item, 'second', id, x)
             })
         ) */
-        /*   this.$nextTick(() => {
+    /*   this.$nextTick(() => {
             console.log(this.$refs)
         }) */
-        console.log(this.$refs)
-    },
+    console.log(this.$refs)
+  },
 }
 </script>
