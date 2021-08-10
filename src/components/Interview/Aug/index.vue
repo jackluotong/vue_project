75e47320abc8d86496a6d85d85aaabff2ceec6ab
WilliamLuo <jackluotong@outlook.com>
2021-08-10 09:49:31 +0800

    complete it.

---
+++ src/components/Interview/Aug/index.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Description: 
+ * @version: 1.0.0
+ * @Author: william
+ * @Date: 2021-08-10 00:13:26
+ * @LastEditors: william
+ * @LastEditTime: 2021-08-10 00:14:12
+ * @For What?: for including interview knowledge.
+-->
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped></style>
