9c8bd9f8a4d4e2be06e79d92f9e376112d23b7bd
WilliamLuo <jackluotong@outlook.com>
2021-09-18 15:44:50 +0800

    condluding base vue knowledge.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,54 @@
+<!--
+ * @Description: 
+ * @version: 1.0.0
+ * @Author: william
+ * @Date: 2021-05-01 08:11:10
+ * @LastEditors: william
+ * @LastEditTime: 2021-09-17 11:10:11
+ * @For What?: 
+-->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <ForNextTick/>
-  </div>
+    <div class="home">
+        <ForNextTick />
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import ForNextTick from '../components/pages/ForNextTick'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-    ForNextTick
-  },
-  mounted(){
-    console.log('this is mounted');
-  }
+    name: 'Home',
+    components: {
+        ForNextTick,
+    },
+    mounted() {
+        console.log('this is mounted')
+        this.testClass()
+    },
+    methods: {
+        testClass() {
+            class A {
+                constructor(name) {
+                    this.name = name
+                    this.say = () => {
+                        console.log(name)
+                    }
+                }
+            }
+            class B extends A {}
+            let a = new A('jack')
+            let b = new B()
+            console.log(a, b)
+            a.say()
+            class C {
+                say(name) {
+                    console.log(name)
+                }
+            }
+            let c = new C()
+            c.say('jack')
+            console.log(c)
+        },
+    },
 }
 </script>
